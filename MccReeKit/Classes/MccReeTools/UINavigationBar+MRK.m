c3d019bb5695481b72b43303c1668178861b2f65
马千里 <maqianli@ysten.com>
2018-04-13 21:39:26 +0800

    update

--- MccReeKit/Classes/MccReeTools/UINavigationBar+MRK.m
+++ MccReeKit/Classes/MccReeTools/UINavigationBar+MRK.m
@@ -6,10 +6,40 @@
 //
 
 #import "UINavigationBar+MRK.h"
-
+#import <objc/runtime.h>
 
 @implementation UINavigationBar (MRK)
 
+static char overlayKey;
 
+- (UIView *)overlay
+{
+    return objc_getAssociatedObject(self, &overlayKey);
+}
+
+- (void)setOverlay:(UIView *)overlay
+{
+    objc_setAssociatedObject(self, &overlayKey, overlay, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (void)cnSetBackgroundColor:(UIColor *)backgroundColor
+{
+    if (!self.overlay) {
+        [self setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
+        self.overlay = [[UIView alloc] initWithFrame:CGRectMake(0, -k_StatusBarHeight, [UIScreen mainScreen].bounds.size.width, k_StatusBarAndNavigationBarHeight)];
+        self.overlay.userInteractionEnabled = NO;
+        self.overlay.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+        [self insertSubview:self.overlay atIndex:0];
+    }
+    
+    self.overlay.backgroundColor = backgroundColor;
+}
+
+- (void)cnReset
+{
+    [self setBackgroundImage:nil forBarMetrics:UIBarMetricsDefault];
+    [self.overlay removeFromSuperview];
+    self.overlay = nil;
+}
 
 @end
