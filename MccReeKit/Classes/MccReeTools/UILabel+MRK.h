0509e462bb48c27e502d6c44e22668fbc9e33b2a
马千里 <maqianli@ysten.com>
2018-07-09 16:37:30 +0800

    更新UILabel的扩展

--- MccReeKit/Classes/MccReeTools/UILabel+MRK.h
+++ MccReeKit/Classes/MccReeTools/UILabel+MRK.h
@@ -9,7 +9,6 @@
 
 @interface UILabel (MRK)
 
-
 /**
  创建UILabel
  @param color 颜色 系统默认
@@ -19,4 +18,41 @@
 + (UILabel *_Nonnull)createLabelColor:(nullable UIColor *)color
                              FontSize:(CGFloat)size;
 
+
+/**
+ 创建UILabel
+ 
+ @param color 颜色
+ @param size 字号
+ @param fontName 字体
+ */
++ (UILabel *_Nonnull)createLabelColor:(nullable UIColor *)color
+                             FontSize:(CGFloat)size
+                             fontName:(NSString *)fontName;
+
+
+
+/**
+ 给个别字加颜色
+ @param str 文本
+ @param color 颜色
+ @param range NSMakerange
+ */
++ (UILabel *_Nullable)createAttrLabel:(NSString *_Nullable)str
+                                color:(UIColor *_Nullable)color
+                           colorRange:(NSRange)range;
+
+
+/**
+ 富文本HTML设置上UILabel
+ @param htmlStr [NSString stringWithFormat:@"<font color='#24A5FE'>2</font><font color='#999999'>/5</font>",@"2"];
+ @return UILabel
+ */
++ (UILabel *_Nullable)createAttrLabelWithHTML:(NSString *_Nullable)htmlStr;
+
+
+/**
+ 设置富文本
+ */
+- (void)setAttrStr:(NSString *)str;
 @end
--- MccReeKit/Classes/MccReeTools/UILabel+MRK.m
+++ MccReeKit/Classes/MccReeTools/UILabel+MRK.m
@@ -9,6 +9,12 @@
 
 @implementation UILabel (MRK)
 
+/**
+ 创建UILabel
+ @param color 颜色 系统默认
+ @param size 字号 系统默认
+ @return UILabel
+ */
 + (UILabel *)createLabelColor:(UIColor *)color
                      FontSize:(CGFloat)size{
     UILabel *label = [[UILabel alloc]init];
@@ -17,6 +23,53 @@
     }
     label.font = [UIFont systemFontOfSize:size];
     return label;
+}
+
+/**
+ 创建UILabel
+ @param color 颜色
+ @param size 字号
+ @param fontName 字体
+ */
++ (UILabel *_Nonnull)createLabelColor:(nullable UIColor *)color
+                             FontSize:(CGFloat)size
+                             fontName:(NSString *)fontName{
+    UILabel *label = [UILabel createLabelColor:color FontSize:size];
+    label.font = [UIFont fontWithName:fontName size:size];
+    return label;
+}
+
+/**
+ 给个别字加颜色
+ @param str 文本
+ @param color 颜色
+ @param range NSMakerange
+ */
++ (UILabel *_Nullable)createAttrLabel:(NSString *_Nullable)str
+                                color:(UIColor *_Nullable)color
+                           colorRange:(NSRange)range{
+    UILabel *label = [[UILabel alloc]init];
+    NSMutableAttributedString *AttributedStr = [[NSMutableAttributedString alloc]initWithString:str];
+    [AttributedStr addAttribute:NSForegroundColorAttributeName value:color range:range];
+    label.attributedText = AttributedStr;
+    return label;
+}
+
+/**
+ 富文本HTML设置上UILabel
+ @param htmlStr [NSString stringWithFormat:@"<font color='#24A5FE'>2</font><font color='#999999'>/5</font>",@"2"];
+ @return UILabel
+ */
++ (UILabel *)createAttrLabelWithHTML:(NSString *)htmlStr{
+    UILabel *label = [[UILabel alloc]init];
+    NSAttributedString * attrStr = [[NSAttributedString alloc] initWithData:[htmlStr dataUsingEncoding:NSUnicodeStringEncoding] options:@{NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType} documentAttributes:nil error:nil];
+    label.attributedText = attrStr;
+    return label;
+}
+
+- (void)setAttrStr:(NSString *)str{
+    NSAttributedString * attrStr = [[NSAttributedString alloc] initWithData:[str dataUsingEncoding:NSUnicodeStringEncoding] options:@{NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType} documentAttributes:nil error:nil];
+    self.attributedText = attrStr;
 }
 
 @end
